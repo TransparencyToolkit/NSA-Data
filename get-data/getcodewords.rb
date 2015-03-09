9346dd8d66cedecd70437009fda9fb943c4add25
Debian Live user <shidash@shidash.com>
2015-03-09 11:53:04 +0000

    Added GCHQ codewords

--- get-data/getcodewords.rb
+++ get-data/getcodewords.rb
@@ -4,24 +4,32 @@ require 'open-uri'
 
 class GetCodewords
   def initialize
-    @codeword_list = Nokogiri::HTML(open("http://electrospaces.blogspot.nl/p/nicknames-and-codewords.html"))
-    @words_and_desc = Array.new
+    @nsa_list = Nokogiri::HTML(open("http://electrospaces.blogspot.nl/p/nicknames-and-codewords.html"))
+    @gchq_list = Nokogiri::HTML(open("http://electrospaces.blogspot.nl/p/gchq-nicknames-and-codewords.html"))
+    @words_and_desc = Hash.new
   end
 
   # Downloads all codewords
-  def download
-    splittext = @codeword_list.text.split("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z").last.split("- See also this list of NSA codewords from 2002").first
+  def downloadNSA
+    splittext = @nsa_list.text.split("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z").last.split("- See also this list of NSA codewords from 2002").first
     splitlist = splittext.split("back to top")
+    getList(splitlist)
+  end
 
-    # Loop through the list for each letter
-    splitlist.each do |letter_list|
+  # Download GCHQ codewords
+  def downloadGCHQ
+    splittext = @gchq_list.text.split("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z").last.split("- See also the codenames of JTRIG tools and techniques").first
+    splitlist = splittext.split("back to top")
+    getList(splitlist)
+  end
+
+  # Loop through the list for each letter
+  def getList(data)
+    data.each do |letter_list|
       letter_list.each_line do |line|
         if line.include? " - "
           ls = line.split(" - ")
-          temphash = Hash.new
-          temphash[:codeword] = ls[0]
-          temphash[:description] = ls[1]
-          @words_and_desc.push(temphash)
+          @words_and_desc[ls[0]] = { codeword: ls[0], description: ls[1]}
         end
       end
     end
@@ -29,18 +37,20 @@ class GetCodewords
 
   # Formatted list with variations
   def makeList
-    download
+    downloadNSA
+    downloadGCHQ
     JSON.pretty_generate(@words_and_desc)
   end
 
   # To do:
-  # Merge with existing list
-  # Key/consistent allcaps version
   # Lowercase where appropriate
+  # Allcaps one word
   # CamelCase
   # With a space
   # Abbreviations in parentheses
   # Any other variations
+  # Remove parentheses from keys
+  # Merge with existing/edited list
 end
 
 g = GetCodewords.new
